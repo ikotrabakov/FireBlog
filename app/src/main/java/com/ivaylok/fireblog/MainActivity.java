fac4f344f463ef44e164c5d0a0a6357663aedcf8
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-09-28 13:28:54 +0300

    Initial commit

---
+++ app/src/main/java/com/ivaylok/fireblog/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ivaylok.fireblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
