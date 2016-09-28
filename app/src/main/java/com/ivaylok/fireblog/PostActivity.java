29dc423166bc9f769f2a5a21c22975788527743a
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-09-28 15:54:48 +0300

    Upload image to firebase storage.

--- app/src/main/java/com/ivaylok/fireblog/PostActivity.java
+++ app/src/main/java/com/ivaylok/fireblog/PostActivity.java
@@ -1,26 +1,47 @@
 package com.ivaylok.fireblog;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import org.w3c.dom.Text;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mPostButton;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
     private static final int GALLERY_REQUEST = 1;
+    private Uri imageUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
         mSelectImage = (ImageButton)findViewById(R.id.add_image);
+        mPostTitle = (EditText)findViewById(R.id.titleField);
+        mPostDesc = (EditText)findViewById(R.id.descField);
+        mPostButton = (Button)findViewById(R.id.submitBtn);
+        mProgress = new ProgressDialog(this);
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,13 +50,38 @@ public class PostActivity extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+
+        mPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to blog ...");
+        mProgress.show();
+
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDesc.getText().toString().trim();
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && imageUri != null) {
+            StorageReference filepath = mStorage.child("Blog_Images").child(imageUri.getLastPathSegment());
+            filepath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             mSelectImage.setImageURI(imageUri);
 
             CropImage.activity(imageUri)
